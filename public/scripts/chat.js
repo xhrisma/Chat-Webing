8c0647a924e3ab5b80a2b80acdfaf3d27b28f216
ChristianTSC <ctorressantacruz98@gmail.com>
2021-05-24 21:38:38 -0500

    arreglos enter

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -33,8 +33,8 @@ function updateUsers(socket){
 
 
 function newMessage(socket){
-    const message=$('#message').val();
-    if(message){
+   
+    if(message==$('#message').val()){
 
     $('#message').keydown(function(ev){
         if(ev.keyCode ==13){
